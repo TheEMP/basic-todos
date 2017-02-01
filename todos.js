70eee7b1168245f67250b6e4966f6d6563b4d9ea
TheEMP <gunnarDebate@gmail.com>
2017-02-01 09:58:15 -0700

    Done!

--- todos.js
+++ todos.js
@@ -2,12 +2,20 @@ const vm = new Vue({
     el: '#root',
     data() {
         return {
-            todos: []
+            todos: [],
+            todo: ""
         }
     },
     methods: {
-        submitTodo(todo) {
-            this.todos.push(todo)
+        submitTodo: function(todo) {
+            this.todos.push(this.todo)
+            this.todo = ""
+        },
+        removeTodo: function(id) {
+            this.todos.splice(id, 1)
+        },
+        clear: function() {
+            this.todo = ""
         }
     }
 })
